af8d462584b10f7d3630b0687336e899e59f14a2
Thomas Morris <tmorris@thisiszone.com>
2015-02-24 21:32:52 +0000

    Add grunt and configure sass goodness

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+	    pkg: grunt.file.readJSON('package.json'),
+	    compass: {
+	        dist: {
+	            options: {
+	                'outputStyle': 'compressed',
+	                'sassDir': '_scss',
+	                'cssDir': 'public/css'
+	            }
+	        }
+	    },
+	    jekyll: {
+		    dist: {
+		        options: {
+			        src: '.',
+			        dest: '_site',
+			        config: '_config.yml',
+			        safe: true
+		        }
+		    }
+	    },
+	    watch: {
+		    //markup: {
+		    //  files: ['index.html','_includes/*.html','_posts/*'],
+		    //  tasks: ['jekyll'],
+		    //  options: {
+		    //    livereload: true
+		    //  }
+            //},
+            css: {
+                files: ['_scss/{,*/}*.{scss,sass}'],
+                tasks: ['compass'],
+                options: {
+	                livereload: true
+                }
+            }
+	    }
+    });
+	 
+    //load npm tasks
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-jekyll');
+
+    grunt.registerTask('default', ['compass']);
+};
